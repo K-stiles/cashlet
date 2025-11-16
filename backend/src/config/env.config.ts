355e67825e0b16e8dcfb9bbfd9585d05de5882bd
K-stiles <michealessuman08@gmail.com>
2025-11-16 00:37:15 +0000

    feat: dockerize api-server and deployed to fly.io

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -3,7 +3,7 @@ import { getEnv } from "../utils/get-env";
 const envConfig = () => ({
   NODE_ENV: getEnv("NODE_ENV", "development"),
 
-  PORT: getEnv("PORT", "8080"),
+  PORT: getEnv("PORT", "3000"),
   BASE_PATH: getEnv("BASE_PATH", "/api"),
   MONGO_URI: getEnv("MONGO_URI"),
 
